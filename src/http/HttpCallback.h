feaa756c4d37f128dd332672d26cee8b22cd2b83
miaoshua <945970809@qq.com>
2015-12-10 18:18:20 +0800

    将传递整个文件内容给HttpResponse改为给其文件的文件描述符

--- src/http/HttpCallback.h
+++ src/http/HttpCallback.h
@@ -6,186 +6,28 @@
     > Created Time: 2015年12月08日 星期二 16时50分43秒
  =======================================================*/
 #include <muduo/net/http/HttpRequest.h>
-
-#include <muduo/base/StringPiece.h>
-
-#include <string>
-#include <vector>
-#include <map>
-
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 #include <muduo/net/http/HttpResponse.h>
 
-//从字符串中截取配置名字和配置内容
-std::pair<std::string,std::string> intercept(const char *str)
-{
-    int i=0;
-    std::pair<std::string,std::string> option;    //保存每行的关键字和其对应的值
+#include <string>
+#include <map>
 
-    while(1)
-    {
-        if(str[i] == ' ')
-        {
-            std::string s1(str,&str[i]);
-            option.first = s1;
-            std::string s2(&str[i] + 3);
-            s2.resize(s2.size() - 1);
-            option.second = s2;
-            return option;   
-        }
-        else
-        {
-            i++;
-        }
-    }
-}
+
+//从字符串中截取配置名字和配置内容
+std::pair<std::string,std::string> intercept(const char *str);
 
 //从字符串中提取map的key和value
-std::pair<std::string,std::string> extract(const char *str)
-{
-    std::pair<std::string,std::string> value;
-
-    for(int i=0;; i++)
-    {
-        if(str[i] == ' ')
-        {
-            std::string s1(str,&str[i]);
-            value.first = s1;
-            std::string s2(&str[i] + 5);
-            
-            s2.resize(s2.size() - 1);
-            value.second = s2;
-            return value;
-        }
-    }
-}
+std::pair<std::string,std::string> extract(const char *str);
 
 
 //获得MIME的map(将包含文件后缀名和对应MIME类型装载成一个map)
-std::map<std::string,std::string> getAllMIME(void)
-{
-    std::map<std::string,std::string> m;
-
-    FILE *fp = fopen("/home/shreck/html/MIME.txt","r");
-    if(!fp)
-    {
-        printf("打开MIME文件出错\n");
-    }
-
-    char buf[1024];
-    while(fgets(buf,sizeof(buf),fp) != NULL)
-    {
-        if(buf[0] == '#')
-        {
-            continue;
-        }
-        else
-        {
-            m.insert(extract(buf));                                        
-        }
-        bzero(buf,sizeof(buf));
-    }
-    fclose(fp);
-    return m;
-}
+std::map<std::string,std::string> getAllMIME(void);
 
 
 //根据key在配置文件中获得对应的属性
-std::string getMessageFromFile(const std::string &key)
-{
-    //打开配置文件
-    FILE *fp = fopen("/home/shreck/Express_help/express_help.conf","r");
-    if(!fp)
-    {
-        printf("打开配置文件失败!\n");
-    }
-    char buf[1024];
-    std::pair<std::string,std::string> option;
-    
-    while(fgets(buf,sizeof(buf),fp) != NULL)
-    {
-        if(buf[0] == '#')
-        {
-            continue;
-        }
-        else
-        {
-            option = intercept(buf);
-            if(option.first == key)
-            {
-                return option.second;
-            }
-        }
-        bzero(&buf,sizeof(buf));
-    }
-    fclose(fp);
-    return NULL;   
-}
-
-
+std::string getMessageFromFile(const std::string &key);
 
 //获得客户端要获取的文件的后缀名
-std::string getContentType(const std::string path)
-{
-    std::string key;
-    for(int i = 0 ;; i++)   //获得要取资源的后缀名
-    {
-        if(path[i] == '.')
-        {
-            std::string t(&path[i],path.size() - i + 1);
-            key = t;
-            break;
-        }   
-    }
-    key.resize(key.size() - 1);
-
-    auto m = getAllMIME();
-
-    auto it = m.find(key);
-    
-    if(it == m.end())
-    {
-        printf("表中没有此后缀名\n");
-    }
-    else
-    {
-        return it->second;
-    }
-}
-
-
+std::string getContentType(const std::string path);
 
 //http服务器回调
-void httpCallback(const muduo::net::HttpRequest &req,muduo::net::HttpResponse *resp)
-{
-    std::string site = getMessageFromFile("webHome");//站点跟目录
-    //获得要获取文件的描述符
-    std::string st(req.path().data());
-    std::string path(site);
-    path = path + st; 
-    std::cout<<path<<std::endl;
-    int fd = open(path.c_str(),O_RDONLY);
-    printf("\nfd = %d\n",fd);
-    if(fd == -1)
-        {
-        resp->setStatusCode(muduo::net::HttpResponse::k404NotFound);
-        resp->setStatusMessage("服务器没有找到您所访问的资源\n");
-        resp->setContentType("text/plain");
-        resp->addHeader("Server","shreck");
-        resp->setCloseConnection(true);
-    }
-    //请求成功
-    else
-    {
-        resp->setStatusCode(muduo::net::HttpResponse::k200Ok);
-        resp->setStatusMessage("OK");
-        resp->setContentType(getContentType(req.path().data()).c_str());
-        resp->addHeader("Server","shreck");
-        resp->setFd(fd);
-        resp->fun();
-        
-    }    
-}
+void httpCallback(const muduo::net::HttpRequest &req,muduo::net::HttpResponse *resp);

39bc635851f6636ce21be6dfc6149ce5b0a2f474
miaoshua <945970809@qq.com>
2015-12-10 17:26:31 +0800

    将其append(body_) 变为边读文件，边添加buffer

--- src/http/HttpResponse.cc
+++ src/http/HttpResponse.cc
@@ -1,49 +1,72 @@
-/*======================================================
-    > File Name: HttpResponse.cc
-    > Author: MiaoShuai
-    > E-mail:  
-    > Other :  
-    > Created Time: 2015年11月28日 星期六 10时30分40秒
- =======================================================*/
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
 
-#include <muduo/net/http/HttpResponse.h>
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+//
+
+#include "HttpResponse.h"
 #include <muduo/net/Buffer.h>
-
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
 
 using namespace muduo;
 using namespace muduo::net;
 
-void HttpResponse::appendToBuffer(Buffer *output)const
+void HttpResponse::appendToBuffer(Buffer* output) const
 {
-    char buf[32];
-    snprintf(buf,sizeof buf,"HTTP/1.1 %d",statusCode_);
-    output->append(buf);
-    output->append(statusMessage_);
+  char buf[32];
+  int size = 0;
+  snprintf(buf, sizeof buf, "HTTP/1.1 %d ", statusCode_);
+  output->append(buf);
+  output->append(statusMessage_);
+  output->append("\r\n");
+
+  if (closeConnection_)
+  {
+    output->append("Connection: close\r\n");
+  }
+  else
+  {
+    //printf("\n**%d**\n",body_.length());
+    //snprintf(buf, sizeof buf, "Content-Length: %zd\r\n", body_.size());
+    //output->append(buf);
+  }
+
+  for (std::map<string, string>::const_iterator it = headers_.begin();
+       it != headers_.end();
+       ++it)
+  {
+    output->append(it->first);
+    output->append(": ");
+    output->append(it->second);
     output->append("\r\n");
+  }
 
-    if(closeConnection_)
+    struct stat stat_buf;
+    fstat(fd_,&stat_buf);
+    snprintf(buf, sizeof buf, "Content-Length: %ld\r\n",stat_buf.st_size);
+   // printf("\nsize = %d\n",size);
+    output->append("Connection: Keep-Alive\r\n");
+    output->append(buf);
+    output->append("\r\n");
+    char data[1024];
+    int ret;
+    
+    while((ret = read(fd_,data,sizeof(data))) > 0)
     {
-        output->append("Connection:close\r\n");
-    }
-    else
-    {
-        //body的长度
-        snprintf(buf,sizeof buf,"Content-Length:%zd\r\n",body_.size());
-        output->append(buf);
-        output->append("Connection: Keep-Alive\r\n");
-    }
-
-    for(std::map<string,string>::const_iterator it = headers_.begin();
-        it != headers_.end();
-        ++it)
-    {
-        output->append(it->first);
-        output->append(": ");
-        output->append(it->second);
-        output->append("\r\n");
+        output->append(data,ret);
+        size += ret;
     }
 
-    output->append(""\r\n);
-    output->append(body);
+    printf("\nsize = %d\n",size);
+    close(fd_);
+    
+    snprintf(buf, sizeof buf, "Content-Length: %d\r\n",size);
+    printf("\nsize = %d\n",size);
+    output->append("Connection: Keep-Alive\r\n");
 }

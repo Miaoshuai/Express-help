ba6ef4038c6b033b7c5383c843905878ed888c6e
miaoshua <945970809@qq.com>
2015-12-10 17:25:16 +0800

    增加变量fd_,并设置其setFd()函数

--- src/http/HttpResponse.h
+++ src/http/HttpResponse.h
@@ -1,13 +1,15 @@
-/*======================================================
-    > File Name: HttpResponse.h
-    > Author: MiaoShuai
-    > E-mail:  
-    > Other :  
-    > Created Time: 2015年11月28日 星期六 09时15分06秒
- =======================================================*/
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/muduo/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
 
-#ifndef MUDUO_NET_HTTPRESPONSE_H
-#define MUDUO_NET_HTTPRESPONSE_H
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+//
+// This is a public header file, it must only include public header files.
+
+#ifndef MUDUO_NET_HTTP_HTTPRESPONSE_H
+#define MUDUO_NET_HTTP_HTTPRESPONSE_H
 
 #include <muduo/base/copyable.h>
 #include <muduo/base/Types.h>
@@ -18,71 +20,67 @@ namespace muduo
 {
 namespace net
 {
-    class Buffer;
-    class HttpResponse : public muduo::copyable
-    {
-        public:
-            enum HttpStatusCode
-            {
-                kUnknow,
-                k200Ok = 200,               //请求成功，实体的主体部分包含了所请求的资源
-                k301MovedPermanently = 301, //资源重定向，响应的Location首部包含了资源现在所处的url
-                k404NotFound = 404,         //服务器无法找到所请求的的资源
-                k500ServerError = 500,      //服务器出错
-            };
-            explicit HttpResponse(bool close)
-                :statusCode_(kUnknow),
-                closeConnection_(close)
-            {
-            
-            }
-            //状态码
-            void setStatusCode(HttpStatusCode code)
-            {
-                statusCode_ = code;
-            }
-            //状态信息
-            void setStatusMessage(const string &message)
-            {
-                statusMessage_ = message;
-            }
-            //是否关闭连接
-            void setCloseConnection(bool on)
-            {
-                closeConnection_ = on;
-            }
 
-            bool closeConnection()const
-            {
-                return closeConnection_;
-            }
+class Buffer;
+class HttpResponse : public muduo::copyable
+{
+ public:
+  enum HttpStatusCode
+  {
+    kUnknown,
+    k200Ok = 200,
+    k301MovedPermanently = 301,
+    k400BadRequest = 400,
+    k404NotFound = 404,
+  };
 
-            //发给客户端数据类型如html或PNG等
-            void setContentType(const string &contentType)
-            {
-                addHeader("Content-Type",contentType);
-            }
+  explicit HttpResponse(bool close)
+    : statusCode_(kUnknown),
+      closeConnection_(close)
+  {
+  }
 
-            //添加头部内容
-            void addHeader(const string &key,const string &value)
-            {
-                headers_[key] = value;
-            }
+  void setStatusCode(HttpStatusCode code)
+  { statusCode_ = code; }
 
-            //添加发送体内容
-            void setBody(const string &body)
-            {
-                body_ = body;
-            }
+  void setStatusMessage(const string& message)
+  { statusMessage_ = message; }
 
-            void appendToBuffer(Buffer *output)const;
+  void setCloseConnection(bool on)
+  { closeConnection_ = on; }
 
-        private:
-            std::map<string,string> headers_;//保存头部
-            HttpStatusCode statusCode_; //响应状态吗
-            string statusMessage_;      //状态码对应的解释
-            bool closeConnection_;      //是否关闭连接
-            string body_;               //数具体
-    }
+  bool closeConnection() const
+  { return closeConnection_; }
+
+ // void setContentType(const string& contentType)
+  //{ addHeader("Content-Type", contentType); }
+
+  // FIXME: replace string with StringPiece
+  void addHeader(const string& key, const string& value)
+  { headers_[key] = value; }
+
+ // void setBody(const string& body)
+  //{ body_ = body; }
+
+  void setFd(int fd)
+  {
+    fd_ = fd;
+  }
+
+  void appendToBuffer(Buffer* output) const;
+
+  
+
+ private:
+  std::map<string, string> headers_;
+  HttpStatusCode statusCode_;
+  // FIXME: add http version
+  string statusMessage_;
+  bool closeConnection_;
+  int fd_;  //所请求资源文件的文件描述符
+};
+
 }
 }
+
+#endif  // MUDUO_NET_HTTP_HTTPRESPONSE_H
